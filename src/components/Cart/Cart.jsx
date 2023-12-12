94940aa22f1b8e6a8fac3895e51c917eda4155d4
Your Name <you@example.com>
2023-12-13 02:15:27 +0600

    Optimize Order Summary and fix issue

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
-import DeleteIcon from '../../images/DeleteIcon.svg'
-import ArrowIcon from '../../images/Arrow.svg'
-const Cart = ({cart, removeCart}) => {
-    
+import DeleteIcon from '../../images/DeleteIcon.svg';
+import ArrowIcon from '../../images/Arrow.svg';
+const Cart = ({ cart, removeCart }) => {
+
     let totalPrice = 0;
     let shipping = 0;
-    for(const product of cart) {
-        totalPrice = totalPrice + product.price;
+    let newQuantity = 0;
+    for (const product of cart) {
+        totalPrice = totalPrice + product.price * product.quantity;
         shipping = shipping + product.shipping;
+        newQuantity = newQuantity + product.quantity;
     }
+
     const tax = totalPrice * 7 / 100;
     const grandTotal = totalPrice + shipping + tax;
     return (
         <div>
             <h4 className="text-gray-800 text-[25px] text-center font-normal font-['Lato']">Order Summary</h4>
-                    <div className="px-[7px] py-12">
-                        <p className="text-slate-700 text-[17px] mb-6 font-normal font-['Lato'] tracking-tight">Selected Items: {cart.length}</p>
-                        <p className="text-slate-700 text-[17px] mb-6 font-normal font-['Lato'] tracking-tight">Total Price: ${totalPrice}</p>
-                        <p className="text-slate-700 text-[17px] mb-6 font-normal font-['Lato'] tracking-tight">Total Shipping Charge: ${shipping}</p>
-                        <p className="text-slate-700 text-[17px] mb-6 font-normal font-['Lato'] tracking-tight">Tax: ${tax.toFixed(2)}</p>
-                        <p className="text-neutral-900 text-[21px] mb-2 font-normal font-['Lato'] tracking-tight">Grand Total: ${grandTotal.toFixed(2)}</p>
-                    </div>
-                    <div className="text-center flex flex-col">
-                        <button onClick={removeCart} className="w-[232px] mb-4 h-12 bg-red-500 rounded text-white text-[17px] font-normal font-['Lato'] tracking-tight flex items-center justify-center gap-3">Clear Cart <img src={DeleteIcon} alt="" /></button>
-                        <button className="w-[232px] h-12 bg-amber-500 rounded text-white text-[17px] font-normal font-['Lato'] tracking-tight flex items-center justify-center gap-3">Review Order <img src={ArrowIcon} alt="" /></button>
-                    </div>
+            <div className="px-[7px] py-12">
+                <p className="text-slate-700 text-[17px] mb-6 font-normal font-['Lato'] tracking-tight">Selected Items: {newQuantity}</p>
+                <p className="text-slate-700 text-[17px] mb-6 font-normal font-['Lato'] tracking-tight">Total Price: ${totalPrice}</p>
+                <p className="text-slate-700 text-[17px] mb-6 font-normal font-['Lato'] tracking-tight">Total Shipping Charge: ${shipping}</p>
+                <p className="text-slate-700 text-[17px] mb-6 font-normal font-['Lato'] tracking-tight">Tax: ${tax.toFixed(2)}</p>
+                <p className="text-neutral-900 text-[21px] mb-2 font-normal font-['Lato'] tracking-tight">Grand Total: ${grandTotal.toFixed(2)}</p>
+            </div>
+            <div className="text-center flex flex-col">
+                <button onClick={removeCart} className="w-[232px] mb-4 h-12 bg-red-500 rounded text-white text-[17px] font-normal font-['Lato'] tracking-tight flex items-center justify-center gap-3">Clear Cart <img src={DeleteIcon} alt="" /></button>
+                <button className="w-[232px] h-12 bg-amber-500 rounded text-white text-[17px] font-normal font-['Lato'] tracking-tight flex items-center justify-center gap-3">Review Order <img src={ArrowIcon} alt="" /></button>
+            </div>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Product from "../Product/Product";
 import { useEffect } from "react";
 import Cart from "../Cart/Cart";
+import { addToDb, deleteShoppingCart, getShoppingCart } from "../../utilities/fakedb";
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -11,14 +12,49 @@ const Shop = () => {
         fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const addProducts = []
+        for(const id in storedCart){
+            const findCartProduct = products.find(product => product.id === id);
+
+            if(findCartProduct){
+                const quantity = storedCart[id];
+                findCartProduct.quantity = quantity;
+                addProducts.push(findCartProduct)
+            } 
+        }
+        setCart(addProducts);
+    }, [products])
+
 
     const addToCart = product => {
-        const newCart = [...cart, product];
+        let newCart = [];
+
+        const exits = cart.find(cartPd => cartPd.id === product.id);
+        
+        if(!exits){
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        else {
+            product.quantity = exits.quantity + 1;
+            const remaining = cart.filter(cartPd => cartPd.id !== product.id);
+            console.log(remaining);
+            newCart = [...remaining, exits];
+        }
+
         setCart(newCart)
+
+        addToDb(product.id)
     }
 
-    const removeCart = () => setCart([])
+    const removeCart = () => {
+        setCart([]);
+        deleteShoppingCart();
+    }
     return (
         <div className="container mx-auto px-8">
             <div className="grid grid-cols-[4fr_1fr]">
