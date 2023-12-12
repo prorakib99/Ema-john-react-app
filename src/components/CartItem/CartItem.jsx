7f8fc0d0c56a8592fcbc3cbdf372ca1fa7cd91f3
Your Name <you@example.com>
2023-12-12 06:20:44 +0600

    Order Review Complete

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -3,17 +3,20 @@ import Trash from '../../images/trash-alt.svg'
 const CartItem = () => {
     return (
         <div>
-            <div className="w-[571px] h-[107px] rounded-lg border flex border-gray-400">
-                <img className="rounded-md m-[8px]" src="https://via.placeholder.com/91x91" />
-                <div className="">
-                    <span className="text-slate-700 text-[15px] font-normal font-['Lato'] tracking-tight">Shipping Charge : </span>
-                    <div className="text-gray-800 text-[21px] font-normal font-['Lato'] tracking-tight">Ultraboost 22 Shoes</div>
-                    <div className="left-[116px] top-[46px] absolute">
-                        <span className="text-gray-800 text-[15px] font-normal font-['Lato'] tracking-tight">Price: </span>
-                        <span className="text-amber-500 text-[15px] font-normal font-['Lato'] tracking-tight">$190 </span>
+            <div className="w-[571px] h-[107px] rounded-lg border flex items-center justify-between border-gray-400">
+                <div className="flex gap-2">
+                    <img className="rounded-md m-[8px]" src="https://via.placeholder.com/91x91" />
+                    <div className='my-3'>
+                        <div className="text-gray-800 text-[21px] font-normal font-['Lato'] tracking-tight">Ultraboost 22 Shoes</div>
+                        <p className="text-gray-800 text-[15px] font-normal font-['Lato'] tracking-tight">Price: <span className="text-amber-500 text-[15px] font-normal font-['Lato'] tracking-tight">$190 </span>
+                        </p>
+                        <p className="text-slate-700 text-[15px] font-normal font-['Lato'] tracking-tight">Shipping Charge : <span className="text-amber-500 text-[15px] font-normal font-['Lato'] tracking-tight">$5</span></p>
+
                     </div>
                 </div>
-                <img src={Trash} className='w-[55px] h-[55px] bg-rose-500 bg-opacity-30 rounded-full' alt="" />
+                <button className='w-[55px] h-[55px] me-6 bg-rose-500 bg-opacity-30 rounded-full flex items-center justify-center'>
+                    <img src={Trash} alt="" />
+                </button>
             </div>
         </div>
     );
